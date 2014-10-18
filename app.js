91c5e4fd8cd616efbeaf99309fcabfe1d766672c
ybot1122 <ybot1122@gmail.com>
2014-10-18 07:19:12 -0700

    massive edits to node server, introduced express framework

--- app.js
+++ app.js
@@ -3,33 +3,120 @@
 */
 
 var port = 9210;
-
-var finalhandler = require('finalhandler');
-var http = require('http');
-var serveStatic = require('serve-static');
-var url = require('url');
-var queryString = require('querystring');
+// express modules
+var express = require('express');
+var app = express();
+var session = require('express-session');
+// external modules
 var mongo = require('mongojs');
 var facebook = require('facebook-sdk');
+var FB = new facebook.Facebook({
+	appId		: 	'1481238075482245',
+	xfbml		: 	true,
+	version		: 	'v2.1'
+});
 
-var serve = serveStatic('.', {});
+/*
+	invoke this function to update a drink tally
+*/
+function updateDrinkCount() {
+	return 0;
+}
 
-var server = http.createServer(function(req, res) {
-	var queryObj = queryString.parse(url.parse(req.url).query);
+/*
+	defines and establishes a new user
+*/
+function createUser() {
+	return 0;
+}
 
-	if (queryObj.hasOwnProperty('hello') && queryObj.hello === 'true') {
-		res.writeHead(200, {'Content-Type': 'application/json'});
-		console.log(JSON.stringify({text: 'yo yo yo', content: 'hello'}));
-		res.end(JSON.stringify({text: 'yo yo yo', content: 'hello'}));
+/*
+	verifies the authenticity of user by checking fb login
+*/
+function fbAuth(fid, accessToken, callback) {
+	// make api call with access token and userID
+	FB.api('/' + fid + '/', 'GET', {access_token: accessToken}, function(response) {
+		if (!response || response.error) {
+			callback(false);
+		} else {
+			callback(true);
+		}
+	});
+}
+
+/*
+	checks if the user is currently in our database
+*/
+function userExists() {
+	return 0;
+}
+
+/*
+	extracts certain statistics and measurements based on the filter
+	object that is passed as an argument
+*/
+function getStatistics() {
+	return 0;
+}
+
+/*
+	wrapper function to make reque
+*/
+function getNutrition() {
+	return 0;
+}
+
+/*
+	returns true only if given client has an active logged session
+*/
+function isInSession(req) {
+	return req.session.hasOwnProperty('logged') && req.session.logged === true;
+}
+
+app.use(session({
+	resave: true,
+	saveUninitialized: true,
+	secret: 'temp',
+	cookie: {maxAge: 5400000}
+}));
+
+// handle get requests
+app.get('/auth/', function(req, res) {
+	if (isInSession(req)) {
+		res.end('already have a session');
 	} else {
-		// no valid GET variables. just serve the HTML file to client.
-		var done = finalhandler(req, res);
-		serve(req, res, done);
+		if (req.query.hasOwnProperty('fid') && req.query.hasOwnProperty('accToken')) {
+			// client wants to authenticate thru facebook
+			fbAuth(req.query.fid, req.query.accToken, function(status) {
+				if (status) {
+					req.session.logged = true;
+					req.session.uid = req.query.fid;
+					res.end('yeah baby');
+				} else {
+					req.session.logged = false;
+					res.end('auth failed');
+				}
+			});
+		}
 	}
 });
 
-// Listen
-server.listen(port);
+app.get('/update/', function(req, res) {
+	if (isInSession(req)) {
+		if (req.query.hasOwnProperty('did') && req.query.hasOwnProperty('action')) {
+			res.end('ok we made the update');
+		} else {
+			res.end('malformed data');
+		}
+	} else {
+		res.end('session error');
+	}
+});
 
-// Put a friendly message on the terminal
-console.log('Server running at http://127.0.0.1:' + port);
+app.use(express.static(__dirname + '/public'));
+
+var server = app.listen(3000, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('Example app listening at http://%s:%s', host, port);
+});
